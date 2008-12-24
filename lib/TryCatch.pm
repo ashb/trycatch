71916e49a3caaae081e060a2892125f74656959b
Ash Berlin <ash_github@firemirror.com>
2008-12-24 13:05:15 -0500

    Start making catch obey TCs and var names

--- lib/TryCatch.pm
+++ lib/TryCatch.pm
@@ -3,8 +3,8 @@ package TryCatch;
 use strict;
 use warnings;
 use Sub::Exporter -setup => {
-  exports => [qw/try catch/],
-  groups => { default => [qw/try catch/] },
+  exports => [qw/try/],
+  groups => { default => [qw/try/] },
   installer => sub {
     my ($args, $to_export) = @_;
     my $pack = $args->{into};
@@ -15,31 +15,41 @@ use Sub::Exporter -setup => {
           { $name => { const => sub { $parser->($pack, @_) } } },
         );
       }
-      if (my $code = __PACKAGE__->can("_extras_for_${name}")) {
-        $code->($pack);
-      }
     }
     Sub::Exporter::default_installer(@_);
 
   }
 };
 
+# Used to detect when there is an explicity return from an eval block
 our $SPECIAL_VALUE = \"no return";
 
 use Devel::Declare ();
 use B::Hooks::EndOfScope;
 use B::Hooks::Parser;
 use Devel::Declare::Context::Simple;
+use SlimSignature;
+use Moose::Util::TypeConstraints;
 
 sub try {}
 
-sub catch {}
+# This might be what catch should be
+sub catch{
+  my ($cond, $err, $tc) = @_;
 
-sub foobar{
-  my ($cond, $err) = @_;
-
+  local $@ = $@;
   local *_ = \$err;
-  return $cond->($err);
+
+  if (defined $tc) {
+    my $type = Moose::Util::TypeConstraints::find_or_create_isa_type_constraint($tc);
+    unless ($type) {
+      warn "Couldn't convert '$tc' to a type constraint";
+      return
+    }
+
+    return unless $type->check($err);
+  }
+  return $err if $cond->($err);
 }
 
 # Replace try with an actual eval call;
@@ -84,8 +94,9 @@ sub try_postlude_block {
   $offset += Devel::Declare::toke_skipspace($offset);
   my $linestr = Devel::Declare::get_linestr();
 
-  my $toke = '';  
-  if (my $len = Devel::Declare::toke_scan_word($offset, 1 )) {
+  my $toke = '';
+  my $len = 0;
+  if ($len = Devel::Declare::toke_scan_word($offset, 1 )) {
     $toke = substr( $linestr, $offset, $len );
   }
 
@@ -94,21 +105,72 @@ sub try_postlude_block {
   my $ctx = Devel::Declare::Context::Simple->new->init($toke, $offset);
 
   if ($toke eq 'catch') {
-    $ctx->inc_offset(length($toke));
-    $ctx->skipspace;
-    my $str = '; if (my $__t_c_error = $@) { my $e = $@; '; 
 
-    #TODO: Check a { is next thing
-    substr( $linestr, $offset, $ctx->offset - $offset +1 ) = $str;
-
+    substr( $linestr, $offset, $len ) = ';';
     $ctx->set_linestr($linestr);
+    $ctx->inc_offset(1);
+    $ctx->skipspace;
+    process_catch($ctx, 1);
 
   } elsif ($toke eq 'finally') {
   } else {
     my $str = '; return $__t_c_ret if !ref($__t_c_ret) || $__t_c_ret != $TryCatch::SPECIAL_VALUE;'; 
     substr( $linestr, $offset, 0 ) = $str;
 
-    Devel::Declare::set_linestr($linestr);
+    $ctx->set_linestr($linestr);
+  }
+}
+
+sub process_catch {
+  my ($ctx, $first) = @_;
+  
+  my $linestr = $ctx->get_linestr;
+
+  if (substr($linestr, $ctx->offset, 1) eq '(') {
+    my ($param, $left) = SlimSignature->param(
+      input => $linestr,
+      offset => $ctx->offset+1 );
+
+    substr($linestr, $ctx->offset, length($linestr) - ($ctx->offset + length($left)), '');
+    $ctx->set_linestr($linestr);
+    $ctx->skipspace;
+
+    if (substr($linestr, $ctx->offset, 1) ne ')') {
+      die "')' expected after catch condition: $linestr\n";
+    }
+    substr($linestr, $ctx->offset, 1, '');
+    $ctx->set_linestr($linestr);
+
+    my $code;
+    $code = 'else ' unless $first;
+
+    $code .= 'if( my '
+           . ($param->{var} || '$e')
+           . ' = TryCatch::catch(';
+    if ($param->{where}) {
+      $code .= $param->{where}[0];
+    } else {
+      $code .= 'sub { 1 }'
+    }
+    $code .= ', $@';
+    if ($param->{tc}) {
+      $code .= ', \'' . $param->{tc} . '\''
+    }
+
+    $code .= ')) ';
+
+    substr($linestr, $ctx->offset, 1) = $code;
+
+    $ctx->set_linestr($linestr);
+  } else {
+    my $str;
+    $str = 'else ' unless $first;
+    $str .= 'if (my $e = $@) { '; 
+
+    #TODO: Check a { is next thing
+    substr( $linestr, $ctx->offset, 1 ) = $str;
+
+    $ctx->set_linestr($linestr);
   }
 }
 1;
